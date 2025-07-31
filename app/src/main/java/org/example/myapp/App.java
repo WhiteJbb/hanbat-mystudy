c02a4e9d060c9cf3748c643cad318a593b24d37c
WhiteJbb <gunni6112@gmail.com>
2025-07-31 15:01:01 +0900

    Spring 기본 웹사이트 출력 연습

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -3,10 +3,18 @@ package org.example.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/*
+// 스프링부트를 실행할 수 있도록 기본 정보를 자동 설정
+// 톰캣 서버 실행에 관련된 기초 설정을 자동으로 처리
+@EnableAutoConfiguration
+// 현재 클래스와 같은 패키지 및 하위 패키지를 다 뒤져서 컴포넌트를 찾아 자동 생성하라는 설정!
+// => @Component, @Controller, @Service, @Repository 등이 붙은 클래스를 찾는다.
+@ComponentScan
+ */
+@SpringBootApplication // = @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
 public class App {
   public static void main(String[] args) {
-    SpringApplication.run(AppConfig.class, args);
+    SpringApplication.run(App.class, args);
     System.out.println("스프링부트 서버 시작!");
   }
 }
