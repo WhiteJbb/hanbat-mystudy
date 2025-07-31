0ae2714618579e150d655960247ffa6a78d7fd27
WhiteJbb <gunni6112@gmail.com>
2025-07-31 13:27:35 +0900

    Spring Boot 서버 기본 설정

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,61 +1,12 @@
 package org.example.myapp;
 
-/**
- * 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다. <br>
- * <code>App</code> 클래스!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-  /**
-   * 애플리케이션의 진입점(entry point) 역할을 하는 메서드입니다. 프로그램을 실행하면 "hello, world"를 콘솔에 출력합니다.
-   *
-   * @param args 프로그램 실행 시 전달되는 아규먼트 배열
-   * @author 한밭대학교
-   * @version 1.0
-   * @since 2025-07-29
-   * @see <a href="https://docs.oracle.com/en/java/javase/21/">JDK Documentation</a>
-   */
   public static void main(String[] args) {
-    //    String result = String.join(",", "aaa", "bbb", "ccc");
-    //    System.out.println(result);
-    //
-    //    int r = Math.abs(-100);
-    //    System.out.println(r);
-    //
-    //    String str = "abcdefghijk";
-    //    System.out.println(str.substring(2, 6));
-    //
-    //    int[] arr1 = new int[] {100, 200, 300, 400, 500};
-    //    int[] arr2 = Arrays.copyOf(arr1, 2);
-    //    for (int v : arr2) {
-    //      System.out.print(v + ",");
-    //    }
-    //    System.out.println();
-    //
-    //    ArrayList list1 = new ArrayList();
-    //    list1.add(100);
-
-    //    Scanner scanner = new Scanner(System.in);
-    //    System.out.print("이름? ");
-    //    String name = scanner.nextLine();
-    //    System.out.printf("안녕하세요 %s 님! \n", name);
-    //    scanner.close();
-
-    class Calculator {
-      int result;
-
-      void plus(int a) {
-        this.result += a;
-      }
-
-      void minus(int a) {
-        this.result -= a;
-      }
-    }
-
-    Calculator c1 = new Calculator();
-    Calculator c2 = new Calculator();
-
-    c1.plus(100);
-    c2.plus(550);
+    SpringApplication.run(AppConfig.class, args);
+    System.out.println("스프링부트 서버 시작!");
   }
 }
---
+++ app/src/main/java/org/example/myapp/AppConfig.java
@@ -0,0 +1,6 @@
+package org.example.myapp;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration // 톰캣 서버 실행에 관련된 기초 설정을 자동으로 처리
+public class AppConfig {}
